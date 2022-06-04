b958b2d89a2da89aff775483b672015ee21fc2bd
muddyguy <mudassar.hussain360@gmail.com>
2022-06-04 15:59:19 -0400

    added api and catfacts

--- holadunya/src/App.js
+++ holadunya/src/App.js
@@ -1,21 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React, { useState, useEffect } from "react";
+import "./App.css";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello world!
+          Hello world! Click link below for some fun!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://catfact.ninja/fact"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          MEOW
         </a>
       </header>
     </div>
